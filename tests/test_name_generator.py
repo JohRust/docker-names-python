5d36a2618f80d3fa39a8e9b6ba0ea5ec211085f8
Johannes Rust <johannes.rust@dfki.de>
2024-03-27 10:27:23 +0100

    formatting

--- tests/test_name_generator.py
+++ tests/test_name_generator.py
@@ -1,10 +1,12 @@
 import docker_names
 
+
 def test_name_formats():
-	random_name = docker_names.get_random_name(retry=0)
-	assert isinstance(random_name, str)
-	assert '_' in random_name
-	
+    random_name = docker_names.get_random_name(retry=False)
+    assert isinstance(random_name, str)
+    assert "_" in random_name
+
+
 def test_name_retry():
-	random_name = docker_names.get_random_name(retry=1)
-	assert int(random_name[-1]) in range(11)
+    random_name = docker_names.get_random_name(retry=True)
+    assert int(random_name[-1]) in range(11)
